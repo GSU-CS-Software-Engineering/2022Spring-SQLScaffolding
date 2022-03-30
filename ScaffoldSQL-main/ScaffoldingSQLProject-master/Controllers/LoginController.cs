87535c9e90cbda328b8b58c6f4ffa03578429a18
AlexisJones0629 <65202890+AlexisJones0629@users.noreply.github.com>
2022-03-30 18:45:30 -0400

    Added Login to Home page

---
+++ ScaffoldSQL-main/ScaffoldingSQLProject-master/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScaffoldingSQLProject.Pages.LoginPage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScaffoldingSQLProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+   //[HttpPost]
+   
+}
--- ScaffoldSQL-main/ScaffoldingSQLProject-master/wwwroot/js/LoginPage.js
+++ ScaffoldSQL-main/ScaffoldingSQLProject-master/wwwroot/js/LoginPage.js
@@ -1,2 +1,5 @@
-﻿function validate() {
-}
+﻿/** function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (usename == "admin" && password =="123abc" )
+}**/
